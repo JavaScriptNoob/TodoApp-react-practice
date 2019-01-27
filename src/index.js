c32b7f66d1120667c3a0adb20fb498bb380a629a
Timur Bobylev <timur.bobylev@yandex.ru>
2019-01-26 17:17:32 -0800

    ccs has been added

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
-import Hedquart from './components/app-header';
-import TodoList from './components/todo-list-js';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
-const App = ()=>{
-    
-    const todoData = [
-{label:'drink cofee', important: false,id:1},
-{label:'Make some noise', important: true,id:2},
-{label:'Have a lunch', important: false,id:3},
+import './index.css';
 
-    ];
-    return (
-    <div>
-        
-        <Hedquart/>
-        <SearchPanel/>
-       <TodoList todos={todoData}/>
-    </div>)
-}
+const App = () => {
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />,
+  document.getElementById('root'));
